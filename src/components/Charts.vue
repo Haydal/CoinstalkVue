eba154152a2ba2a197f5fa9ae393ade80aed2501
Haydal <Haydal69@gmail.com>
2020-12-04 18:44:26 +0100

    Ajout Charts TradingView

--- src/components/Charts.vue
+++ src/components/Charts.vue
@@ -1,9 +1,32 @@
 <template>
-    <h1>Charts</h1>
+<div id="Charts">
+    <VueTradingView :options="{
+       width: 1200,
+       height: 800,
+      symbol: 'COINBASE:BTCUSD',
+      timezone: 'Etc/UTC',
+      theme: 'dark',
+      style: '1',
+      locale: 'en',
+      toolbag_bg: '#f1f3f6',
+      enable_publishing: false,
+      range: 'ALL',
+      hide_side_toolbar: false,
+      allow_symbol_change: true,
+    }" />
+</div>
 </template>
 
 <script>
+import VueTradingView from 'vue-trading-view';
+
 export default {
-    name: 'Charts',
+  name: 'Charts',
+  props: {
+    msg: String
+  },
+  components: {
+    VueTradingView,
+  }
 }
 </script>
