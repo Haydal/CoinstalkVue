6282db2feb1635b3a85dd8701a20281348c1c153
Haydal <Haydal69@gmail.com>
2020-12-04 23:10:32 +0100

    Responsive Charts

--- src/components/Charts.vue
+++ src/components/Charts.vue
@@ -1,33 +1,41 @@
 <template>
-
-<div class="content row">
-  <div class="col-lg-8">
-<VueTradingView 
-    :options="{
-      width: '-webkit-fill-available',
-      height: 800,
-      symbol: 'COINBASE:BTCUSD',
-      timezone: 'Etc/UTC',
-      theme: 'dark',
-      style: '1',
-      locale: 'en',
-      toolbag_bg: '#f1f3f6',
-      enable_publishing: false,
-      range: 'ALL',
-      hide_side_toolbar: false,
-      allow_symbol_change: true,
-    }"
-  /> 
+  <div class="content row">
+    <div class="col-lg-8">
+      <VueTradingView
+        :options="{
+            backgroundColor: 'white',
+            textColor: 'rgba(255, 255, 255, 0.9)',
+          width: '-webkit-fill-available',
+          height: 800,
+          symbol: 'COINBASE:BTCUSD',
+          timezone: 'Etc/UTC',
+          theme: 'dark',
+          style: '1',
+          locale: 'en',
+          toolbag_bg: '#f1f3f6',
+          enable_publishing: false,
+          range: 'ALL',
+          hide_side_toolbar: false,
+          allow_symbol_change: true,
+        }"
+      />
+    </div>
+    <div class="col-lg-4">
+      <h1>Bitcoin</h1>
+      <br />
+      <h6>Price:</h6>
+      <br />
+      <h6>Volume (24):</h6>
+      <br />
+      <h6>Total Market Cap:</h6>
+      <br />
+      <h6>Total supply:</h6>
+      <br />
+      <h6>Circulating supply:</h6>
+      <br />
+      <h2>About #Bitcoin:</h2>
+    </div>
   </div>
-  <div class="col-lg-4 "><h1>Bitcoin</h1><br> 
-  <h6>Price:</h6> <br>
-   <h6>Volume (24):</h6> <br> 
-  <h6>Total Market Cap:</h6><br>
-   <h6>Total supply:</h6><br>
-   <h6>Circulating supply:</h6><br>
-   <h2>About #Bitcoin:</h2> </div>
-
-</div>
 </template>
 
 <script>
